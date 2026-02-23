57b8db18c45b806085661e294c3e319113ce0cd0
jose <jgijalm388@g.educaand.es>
2026-02-23 13:23:07 +0100

    refactor: add CashflowItem and CashflowItemList components for managing income and expenses

---
+++ frontend/src/ui/components/planning/CashflowItemList.tsx
@@ -0,0 +1,47 @@
+import { PlusCircle } from "lucide-react"
+import { CashflowItem } from "./CashflowItem"
+
+export const CashflowItemList = () => {
+    return (
+        <div className="flex flex-col gap-6 col-span-12 lg:col-span-7 bg-card-light rounded-2xl border border-slate-200 p-6">
+            <div className="flex items-center gap-2.5 justify-between">
+                <h3 className="font-bold text-lg">Entradas de Efectivo y Gastos</h3>
+                <button
+                    // onClick={onAddItem}
+                    className="flex items-center gap-1 text-xs font-bold text-primary hover:bg-primary/5 px-3 py-1.5 rounded-lg border border-primary/20 transition-all cursor-pointer"
+                >
+                    <PlusCircle size={18} />
+                    <span className="hidden sm:inline">
+                        NUEVO ÍTEM
+                    </span>
+                </button>
+            </div>
+
+            <div className="flex flex-col gap-3">
+                {/* {items.length > 0 */}
+                {/* ? items.map((item) => ( */}
+                <CashflowItem
+                    key={1}
+                    type="income"
+                    name="Salario"
+                    category="Trabajo"
+                    frequency="Mensual"
+                    amount={2500.00}
+                // onDelete={() => onDeleteItem?.(item.id)}
+                />
+                <CashflowItem
+                    key={2}
+                    type="expense"
+                    name="Alquiler"
+                    category="Hogar"
+                    frequency="Mensual"
+                    amount={500.00}
+                // onDelete={() => onDeleteItem?.(item.id)}
+                />
+                {/* )) :
+                <p className="text-sm text-slate-500">No hay ítems de efectivo registrados. ¡Agrega uno para empezar a planificar tu futuro financiero!</p>
+                } */}
+            </div>
+        </div>
+    )
+}
