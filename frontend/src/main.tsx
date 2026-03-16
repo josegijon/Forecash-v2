f493da392908b77706029c3ab95686a8fc1aca28
jose <jgijalm388@g.educaand.es>
2026-03-16 20:57:50 +0100

    feat: install vercel analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import { router } from './routes'
+import { Analytics } from "@vercel/analytics/react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+import './index.css';
+import { router } from './routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </StrictMode>,
 )
